917a6f2fea9275c10ef819ead23ed54aadead60e
Warrior <react.js.warrior@gmail.com>
2021-08-17 15:18:28 +0300

    fixes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,20 +3,13 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCyTaLRh1bTeyvWQAAYePY17_7EdZEL6ss',
-  authDomain: 'clever-to-do.firebaseapp.com',
-  projectId: 'clever-to-do',
-  storageBucket: 'clever-to-do.appspot.com',
-  messagingSenderId: '995249906170',
-  appId: '1:995249906170:web:859c37eb02a8c17edbc443',
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 }
 
 firebase.initializeApp(firebaseConfig)
 export const db = firebase.firestore()
-
-// apiKey: process.env.REACT_APP_APIKEY,
-//   authDomain: process.env.REACT_APP_AUTHDOMAIN,
-//   projectId: process.env.REACT_APP_PROJECTID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_APP_ID,
